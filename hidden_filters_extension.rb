6be3e7cd81d62bae4983fb27159bd581a31185d7
M. Scott Ford <scott@vaderpi.com>
2010-04-02 16:32:33 -0400

    Updated description, version number, and url.

--- hidden_filters_extension.rb
+++ hidden_filters_extension.rb
@@ -1,23 +1,11 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application_controller'
-
 require_dependency "application_helper"
 require_dependency "#{File.expand_path(File.dirname(__FILE__))}/lib/application_helper_ext"
 
 class HiddenFiltersExtension < Radiant::Extension
-  version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/hidden_filters"
-
-  # define_routes do |map|
-  #   map.namespace :admin, :member => { :remove => :get } do |admin|
-  #     admin.resources :hidden_filters
-  #   end
-  # end
-  
+  version "0.1"
+  description "Radiant extension that adds the ability to disable the selection of a set of text filters."
+  url "http://github.com/mscottford/radiant-hidden-filters-extension"
+    
   def activate
-    o = Object.new
-    o.extend(ApplicationHelper)
-    puts o.timestamp(Time.now)
   end
 end
